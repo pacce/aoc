6bb8477838bb4de8df033a8a281c22ba663dc012
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2022-05-09 11:36:45 -0300

    feat: solves day-03 part two

--- 2021/python/day-03/main.py
+++ 2021/python/day-03/main.py
@@ -6,30 +6,75 @@ def condition(line):
     return np.array([int(c) for c in line])
 
 
+def decimal(xs):
+    sum = 0
+    for (i, x) in enumerate(reversed(xs)):
+        sum += (x * (2 ** i))
+    return sum
+
+
 def gamma(xs):
     ss      = np.sum(xs, axis=0)
     length  = int(xs.shape[0] / 2.0)
-    binary  = reversed([1 if s > length else 0 for s in ss])
+    binary  = [1 if s > length else 0 for s in ss]
 
-    sum = 0
-    for (i, b) in enumerate(binary):
-        sum += (b * (2 ** i))
-    return sum
+    return decimal(binary)
 
 
 def epsilon(xs):
     ss      = np.sum(xs, axis=0)
     length  = int(xs.shape[0] / 2.0)
-    binary  = reversed([0 if s > length else 1 for s in ss])
+    binary  = [0 if s > length else 1 for s in ss]
 
-    sum = 0
-    for (i, b) in enumerate(binary):
-        sum += (b * (2 ** i))
-    return sum
+    return decimal(binary)
+
+
+def oxygen(xs, index=0):
+    x = xs[:, index]
+
+    ss      = np.sum(xs, axis=0)
+    length  = xs.shape[0] / 2.0
+
+    if ss[index] >= length:
+        bit = 1
+    else:
+        bit = 0
+
+    location    = np.where(x == bit)[0]
+    xs          = xs[location]
+
+    if xs.shape[0] == 1:
+        return decimal(xs.ravel())
+    else:
+        return oxygen(xs, index + 1)
+
+
+def carbon(xs, index=0):
+    x = xs[:, index]
+
+    ss      = np.sum(xs, axis=0)
+    length  = xs.shape[0] / 2.0
+
+    if ss[index] >= length:
+        bit = 0
+    else:
+        bit = 1
+
+    location    = np.where(x == bit)[0]
+    xs          = xs[location]
+
+    if xs.shape[0] == 1:
+        return decimal(xs.ravel())
+    else:
+        return carbon(xs, index + 1)
 
 
 def fst(xs):
     return gamma(xs) * epsilon(xs)
+
+
+def snd(xs):
+    return oxygen(xs) * carbon(xs)
 
 
 def main(args):
@@ -37,6 +82,7 @@ def main(args):
         lines = [x.split()[0] for x in fd.readlines()]
     xs = np.stack([condition(line) for line in lines])
     print(fst(xs))
+    print(snd(xs))
 
 
 if __name__ == '__main__':
