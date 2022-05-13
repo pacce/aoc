a33baf472da13d9aa46a032f8dc46890517ae778
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2022-05-13 13:08:31 -0300

    feat: solves day 7, part 2

--- 2021/python/day-07/main.py
+++ 2021/python/day-07/main.py
@@ -2,6 +2,10 @@ from argparse import ArgumentParser
 import numpy as np
 
 
+def gauss(n):
+    return (n * (n + 1)) / 2
+
+
 def first(ps):
     pmax    = np.max(ps)
 
@@ -13,9 +17,23 @@ def first(ps):
     return int(xs[best])
 
 
+def second(ps):
+    pmax    = np.max(ps)
+
+    xs  = np.zeros(pmax)
+    for i in range(pmax):
+        cs  = np.abs(ps - i)
+        for c in cs:
+            xs[i] += gauss(c)
+    best = np.argmin(xs)
+    return int(xs[best])
+
+
+
 def main(args):
     ps  = np.loadtxt(args.file, delimiter=',', dtype=int)
     print('{}'.format(first(ps)))
+    print('{}'.format(second(ps)))
 
 
 if __name__ == '__main__':
